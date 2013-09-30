480a7b043c25c79459264ca4a4f77154c1022f1d
beyonehu <beyonehu@beyonehutekiMac-mini.local>
2013-09-30 23:51:28 +0800

    Initial Commit

---
+++ test0930_1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test0930_1
+//
+//  Created by beyonehu on 13-9-30.
+//  Copyright (c) 2013年 beyonehu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
